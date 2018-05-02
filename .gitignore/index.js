0007ce5837247747a31382b24345343d18bc0a79
Tommy-59 <34315818+Tommy-59@users.noreply.github.com>
2018-05-02 19:55:13 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.login('NDMyMjIxNzIyNTE1NzM0NTQ4.DaqJ3Q.LqggSoCvrKoX7Op3cdDgL1tKTQU')
+
+
+  bot.login('NDMyMjIxNzIyNTE1NzM0NTQ4.DaqJ3Q.LqggSoCvrKoX7Op3cdDgL1tKTQU');
+
+  bot.on("ready" , () => {
+    bot.user.setPresence({ game: { name: 'Développement ', type: 0} })
+    console.log("Le bot a bien été connecté !");
+});
