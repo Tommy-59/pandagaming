2d9f69f0aee2ba130c45d1bcbf09dbfef767d972
Tommy-59 <34315818+Tommy-59@users.noreply.github.com>
2018-06-10 10:17:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login('NDMyMjIxNzIyNTE1NzM0NTQ4.DaqJ3Q.LqggSoCvrKoX7Op3cdDgL1tKTQU')
-
-
-  bot.login('NDMyMjIxNzIyNTE1NzM0NTQ4.DaqJ3Q.LqggSoCvrKoX7Op3cdDgL1tKTQU');
 
   bot.on("ready" , () => {
     bot.user.setPresence({ game: { name: 'En Développement ', type: 0} })
